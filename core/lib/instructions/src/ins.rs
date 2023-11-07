637d0f3cc8bde8df54e655898362961f986c9c61
Aaron Christiansen <aaronc20000@gmail.com>
2023-11-07 21:42:27 +0000

    Add encodings for remaining instructions
    This took... a while

--- core/lib/instructions/src/ins.rs
+++ core/lib/instructions/src/ins.rs
@@ -31,7 +31,37 @@ pub enum Instruction {
     Movsi { src: GPR, dest: SPR },
     Spadd { val: GPR },
 
-    // TODO
+    // Bit Manipulation
+    Not { reg: GPR },
+    And { reg: GPR, val: GPR },
+    Or { reg: GPR, val: GPR },
+    Xor { reg: GPR, val: GPR },
+    Shl { reg: GPR, val: GPR },
+    Shr { reg: GPR, val: GPR },
+
+    // General-Purpose Arithmetic
+    Neg { reg: GPR },
+    Inc { reg: GPR },
+    Dec { reg: GPR },
+    Add { reg: GPR, val: GPR },
+    Sub { reg: GPR, val: GPR },
+    Mulu { reg: GPR, val: GPR },
+    Muli { reg: GPR, val: GPR },
+
+    // Comparison
+    Inv,
+    Eqz { reg: GPR },
+    Eq { left: GPR, right: GPR },
+    Gt { left: GPR, right: GPR },
+    Gteq { left: GPR, right: GPR },
+
+    // Branching
+    Jmpoff { offset: u8 },
+    Cjmpoff { offset: u8 },
+    Jmp { src: GPR },
+    Cjmp { src: GPR },
+    Call { src: GPR },
+    Ret,
 }
 
 impl Instruction {
@@ -78,6 +108,60 @@ impl Encodable for Instruction {
                 => 0b_0010_0001_1100_0000 | dest.encode() << 4 | src.encode(),
             Instruction::Spadd { val }
                 => 0b_0010_0001_1101_1000 | val.encode(),
+
+            // Bit Manipulation
+            Instruction::Not { reg }
+                => 0b_0100_0000_0000_0000 | reg.encode(),
+            Instruction::And { reg, val }
+                => 0b_0100_0001_0000_0000 | val.encode() << 4 | reg.encode(),
+            Instruction::Or { reg, val }
+                => 0b_0100_0010_0000_0000 | val.encode() << 4 | reg.encode(),
+            Instruction::Xor { reg, val }
+                => 0b_0100_0011_0000_0000 | val.encode() << 4 | reg.encode(),
+            Instruction::Shl { reg, val }
+                => 0b_0100_0100_0000_0000 | val.encode() << 4 | reg.encode(),
+            Instruction::Shr { reg, val }
+                => 0b_0100_0101_0000_0000 | val.encode() << 4 | reg.encode(),
+
+            // General-Purpose Arithmetic
+            Instruction::Neg { reg }
+                => 0b_0100_1000_0000_0000 | reg.encode(),
+            Instruction::Inc { reg }
+                => 0b_0100_1000_0001_0000 | reg.encode(),
+            Instruction::Dec { reg }
+                => 0b_0100_1000_0010_0000 | reg.encode(),
+            Instruction::Add { reg, val }
+                => 0b_0100_1001_0000_0000 | val.encode() << 4 | reg.encode(),
+            Instruction::Sub { reg, val }
+                => 0b_0100_1010_0000_0000 | val.encode() << 4 | reg.encode(),
+            Instruction::Mulu { reg, val }
+                => 0b_0100_1011_0000_0000 | val.encode() << 4 | reg.encode(),
+            Instruction::Muli { reg, val }
+                => 0b_0100_1011_1000_0000 | val.encode() << 4 | reg.encode(),
+
+            // Comparison
+            Instruction::Inv => 0b_0101_0000_0000_0000,
+            Instruction::Eqz { reg }
+                => 0b_0101_0000_0001_0000 | reg.encode(),
+            Instruction::Eq { left, right }
+                => 0b_0101_0001_0000_0000 | left.encode() << 4 | right.encode(),
+            Instruction::Gt { left, right }
+                => 0b_0101_0010_0000_0000 | left.encode() << 4 | right.encode(),
+            Instruction::Gteq { left, right }
+                => 0b_0101_0011_0000_0000 | left.encode() << 4 | right.encode(),
+
+            // Branching
+            Instruction::Jmpoff { offset }
+                => 0b_0110_0000_0000_0000 | offset as u16,
+            Instruction::Cjmpoff { offset }
+                => 0b_0110_0001_0000_0000 | offset as u16,
+            Instruction::Jmp { src }
+                => 0b_0110_0010_0000_0000 | src.encode(),
+            Instruction::Cjmp { src } 
+                => 0b_0110_0011_0000_0000 | src.encode(),
+            Instruction::Call { src }
+                => 0b_0110_0010_0001_0000 | src.encode(),
+            Instruction::Ret => 0b_0110_0010_0001_1000,
         }
     }
 
@@ -108,6 +192,38 @@ impl Encodable for Instruction {
                 "0010_0001_10ss_0ddd" => Movso { src: SPR::decode(s)?, dest: GPR::decode(d)? },
                 "0010_0001_11dd_0sss" => Movsi { src: GPR::decode(s)?, dest: SPR::decode(d)? },
                 "0010_0001_1101_1ooo" => Spadd { val: GPR::decode(o)? },
+
+                // Bit Manipulation
+                "0100_0000_0000_0rrr" => Not { reg: GPR::decode(r)? },
+                "0100_0001_0xxx_0rrr" => And { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+                "0100_0010_0xxx_0rrr" => Or { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+                "0100_0011_0xxx_0rrr" => Xor { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+                "0100_0100_0xxx_0rrr" => Shl { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+                "0100_0101_0xxx_0rrr" => Shr { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+
+                // General-Purpose Arithmetic
+                "0100_1000_0000_0rrr" => Neg { reg: GPR::decode(r)? },
+                "0100_1000_0001_0rrr" => Inc { reg: GPR::decode(r)? },
+                "0100_1000_0010_0rrr" => Dec { reg: GPR::decode(r)? },
+                "0100_1001_0xxx_0rrr" => Add { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+                "0100_1010_0xxx_0rrr" => Sub { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+                "0100_1011_0xxx_0rrr" => Mulu { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+                "0100_1011_1xxx_0rrr" => Muli { reg: GPR::decode(r)?, val: GPR::decode(x)? },
+
+                // Comparison
+                "0101_0000_0000_0000" => Inv,
+                "0101_0000_0001_0rrr" => Eqz { reg: GPR::decode(r)? },
+                "0101_0001_0aaa_0bbb" => Eq { left: GPR::decode(a)?, right: GPR::decode(b)? },
+                "0101_0010_0aaa_0bbb" => Gt { left: GPR::decode(a)?, right: GPR::decode(b)? },
+                "0101_0011_0aaa_0bbb" => Gteq { left: GPR::decode(a)?, right: GPR::decode(b)? },
+
+                // Branching
+                "0110_0000_bbbb_bbbb" => Jmpoff { offset: b as u8 },
+                "0110_0001_bbbb_bbbb" => Cjmpoff { offset: b as u8 },
+                "0110_0010_0000_0rrr" => Jmp { src: GPR::decode(r)? },
+                "0110_0011_0000_0rrr" => Cjmp { src: GPR::decode(r)? },
+                "0110_0010_0001_0rrr" => Call { src: GPR::decode(r)? },
+                "0110_0010_0001_1000" => Ret,
 
                 _ => return None,
             }
