f0da3a764f082a92e82aa33d2b4111370efb2385
Aaron Christiansen <aaronc20000@gmail.com>
2023-11-07 21:51:05 +0000

    Break ins into files

---
+++ core/lib/instructions/src/ins/analysis.rs
@@ -0,0 +1,11 @@
+use crate::{AnyRegister, Instruction};
+
+impl Instruction {
+    pub fn read_registers(self) -> Vec<AnyRegister> {
+        todo!();
+    }
+
+    pub fn written_registers(self) -> Vec<AnyRegister> {
+        todo!();
+    }
+}
--- core/lib/instructions/src/ins/encoding.rs
+++ core/lib/instructions/src/ins/encoding.rs
@@ -1,78 +1,6 @@
-//! A machine representation of the instructions described in design/instructions.md.
-//! 
-//! This implementation should always match that one.
-//! Whenever a complete list of instructions is specified somewhere (e.g. in a match), it should
-//! use the same categories as used in that document, to aid traceability.
-
 use bitmatch::bitmatch;
 
-use crate::{GPR, SPR, DR, AnyRegister, Encodable};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Instruction {
-    // Core
-    Nop,
-    Hlt,
-    Mov { src: GPR, dest: GPR },
-    DMov { src: DR, dest: DR },
-
-    // Immediate Loads
-    Putl { reg: GPR, imm: u8 },
-    Puth { reg: GPR, imm: u8 },
-
-    // Memory
-    Read { addr: GPR, val: GPR },
-    Write { addr: GPR, val: GPR },
-    DRead { addr: GPR, val: DR },
-    DWrite { addr: GPR, val: DR },
-
-    // Special-purpose Registers
-    Movso { src: SPR, dest: GPR },
-    Movsi { src: GPR, dest: SPR },
-    Spadd { val: GPR },
-
-    // Bit Manipulation
-    Not { reg: GPR },
-    And { reg: GPR, val: GPR },
-    Or { reg: GPR, val: GPR },
-    Xor { reg: GPR, val: GPR },
-    Shl { reg: GPR, val: GPR },
-    Shr { reg: GPR, val: GPR },
-
-    // General-Purpose Arithmetic
-    Neg { reg: GPR },
-    Inc { reg: GPR },
-    Dec { reg: GPR },
-    Add { reg: GPR, val: GPR },
-    Sub { reg: GPR, val: GPR },
-    Mulu { reg: GPR, val: GPR },
-    Muli { reg: GPR, val: GPR },
-
-    // Comparison
-    Inv,
-    Eqz { reg: GPR },
-    Eq { left: GPR, right: GPR },
-    Gt { left: GPR, right: GPR },
-    Gteq { left: GPR, right: GPR },
-
-    // Branching
-    Jmpoff { offset: u8 },
-    Cjmpoff { offset: u8 },
-    Jmp { src: GPR },
-    Cjmp { src: GPR },
-    Call { src: GPR },
-    Ret,
-}
-
-impl Instruction {
-    pub fn read_registers(self) -> Vec<AnyRegister> {
-        todo!();
-    }
-
-    pub fn written_registers(self) -> Vec<AnyRegister> {
-        todo!();
-    }
-}
+use crate::{Instruction, Encodable, GPR, DR, SPR};
 
 impl Encodable for Instruction {
     fn encode(self) -> u16 {
@@ -233,12 +161,17 @@ impl Encodable for Instruction {
     }
 }
 
-#[test]
-fn test_brute_mirror_encode_decode() {
-    for bits in 0..u16::MAX {
-        if let Some(ins) = Instruction::decode(bits) {
-            let reencoded = ins.encode();
-            assert_eq!(bits, reencoded);
+#[cfg(test)]
+mod test {
+    use crate::{Instruction, Encodable};
+
+    #[test]
+    fn test_brute_mirror_encode_decode() {
+        for bits in 0..u16::MAX {
+            if let Some(ins) = Instruction::decode(bits) {
+                let reencoded = ins.encode();
+                assert_eq!(bits, reencoded);
+            }
         }
     }
 }
