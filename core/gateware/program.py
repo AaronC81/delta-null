711c330e2ca299034f18a0f90bedf0ac11d4da43
Aaron Christiansen <aaronc20000@gmail.com>
2023-11-15 20:42:20 +0000

    Make program.py able to just build

--- core/gateware/program.py
+++ core/gateware/program.py
@@ -1,4 +1,15 @@
 from src.modules.top import Top
 from src.platform.tinyfpga_bx import TinyFPGABXSerialProgPlatform
+import sys
 
-TinyFPGABXSerialProgPlatform().build(Top(), do_program=True)
+match sys.argv:
+    case [_, "program"]:
+        program = True
+    case [_, "build"]:
+        program = False
+    case _:
+        name = sys.argv[0]
+        print(f"Usage: {name} program/build")
+        sys.exit(1)
+
+TinyFPGABXSerialProgPlatform().build(Top(), do_program=program)
