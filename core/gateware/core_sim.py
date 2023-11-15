5af816b7fcdd1718d47f922b1f12c8a560b8b24a
Aaron Christiansen <aaronc20000@gmail.com>
2023-11-15 18:51:47 +0000

    Add blink example program

--- core/gateware/core_sim.py
+++ core/gateware/core_sim.py
@@ -3,14 +3,12 @@ from amaranth.sim import Simulator
 from src.modules.harness import CoreSimHarness
 
 def bench():
-    for _ in range(100):
+    for _ in range(1000):
         yield
 
-sim = Simulator(CoreSimHarness(instructions=[
-    0x1034, # putl r0, 0x34
-    0x1812, # puth r0, 0x12
-    0xFFFF, # hlt
-]))
+sim = Simulator(CoreSimHarness(instructions=
+    [int(x, 16) for x in "4300 1101 1900 1210 1A00 1307 1B00 4A12 5012 5000 6303 4000 1203 1A00 21C2".split()]
+))
 sim.add_clock(1e-6) # 1 MHz
 sim.add_sync_process(bench)
 with sim.write_vcd("core.vcd"):
--- core/gateware/src/modules/top.py
+++ core/gateware/src/modules/top.py
@@ -6,15 +6,9 @@ class Top(Elaboratable):
         m = Module()
 
         # TODO: use proper memory
-        core_harness = CoreSimHarness(instructions=[
-            # putl r2, data/lo
-            # puth r2, data/hi
-            # read r1, r2
-            # read r0, r2
-            # hlt
-            # data: .word 1
-            0x1205, 0x1A00, 0x2021, 0x2020, 0xFFFF, 0x0001
-        ])
+        core_harness = CoreSimHarness(instructions=
+            [int(x, 16) for x in "4300 1101 1900 12FF 1AFF 1307 1B00 4A12 5012 5000 6303 4000 1203 1A00 21C2".split()]
+        )
         m.submodules += core_harness
 
         # Bind LED to lowest bit of r0
