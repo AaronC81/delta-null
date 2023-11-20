f503b732be5aad024061b8a96ecc67dd528ef334
Aaron Christiansen <aaronc20000@gmail.com>
2023-11-20 21:13:17 +0000

    Add `r` command to keep running until `p`ause

--- core/bin/emulator-frontend/src/main.rs
+++ core/bin/emulator-frontend/src/main.rs
@@ -36,21 +36,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let Response::UpdatedState { state: mut emulator } = backend.send_request(&Request::GetState)? else {
         panic!("GetState error")
     };
-    let mut changes = vec![];
+    
+    let mut state = ApplicationState {
+        emulator,
+        changes: vec![],
+        socket: backend,
+    };
 
     loop {
-        terminal.draw(|f| {
-            let state = ApplicationState {
-                emulator: &emulator,
-                changes: &changes,
-                socket: &backend,
-            };
-            draw(f, &state)
-        }).unwrap();
+        terminal.draw(|f| { draw(f, &state) }).unwrap();
 
         if let Event::Key(key) = event::read()? {
             match key.code {
-                KeyCode::Char('s') => emulator_step(&backend, &mut emulator, &mut changes)?,
+                KeyCode::Char('s') => state.emulator_step()?,
 
                 KeyCode::Char('r') => {
                     loop {
@@ -60,16 +58,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                             }
                         }
 
-                        emulator_step(&backend, &mut emulator, &mut changes)?;
+                        state.emulator_step()?;
 
-                        terminal.draw(|f| {
-                            let state = ApplicationState {
-                                emulator: &emulator,
-                                changes: &changes,
-                                socket: &backend,
-                            };
-                            draw(f, &state)
-                        }).unwrap();
+                        terminal.draw(|f| draw(f, &state)).unwrap();
                     }
                 }
 
@@ -87,34 +78,36 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn emulator_step(backend: &BackendSocket, emulator: &mut EmulatorState, changes: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
-    // Single-step
-    let Response::UpdatedState { state: new_state } = backend.send_request(&Request::ExecuteOneInstruction)? else {
-        panic!("back-end error")
-    };
-
-    // Find changes
-    changes.clear();
-    for (i, (old, new)) in emulator.gprs.iter().zip(new_state.gprs).enumerate() {
-        if *old != new {
-            changes.push(format!("r{i}"));
-        }
-    }
-    if emulator.ip != new_state.ip { changes.push("ip".to_string()); }
-    if emulator.rp != new_state.rp { changes.push("rp".to_string()); }
-    if emulator.sp != new_state.sp { changes.push("sp".to_string()); }
-    if emulator.ef != new_state.ef { changes.push("ef".to_string()); }
-
-    // Swap out state
-    *emulator = new_state;
-
-    Ok(())
+pub struct ApplicationState {
+    emulator: EmulatorState,
+    changes: Vec<String>,
+    socket: BackendSocket,
 }
 
-pub struct ApplicationState<'a> {
-    emulator: &'a EmulatorState,
-    changes: &'a [String],
-    socket: &'a BackendSocket,
+impl ApplicationState {
+    pub fn emulator_step(&mut self) -> Result<(), Box<dyn Error>> {
+        // Single-step
+        let Response::UpdatedState { state: new_state } = self.socket.send_request(&Request::ExecuteOneInstruction)? else {
+            panic!("back-end error")
+        };
+
+        // Find changes
+        self.changes.clear();
+        for (i, (old, new)) in self.emulator.gprs.iter().zip(new_state.gprs).enumerate() {
+            if *old != new {
+                self.changes.push(format!("r{i}"));
+            }
+        }
+        if self.emulator.ip != new_state.ip { self.changes.push("ip".to_string()); }
+        if self.emulator.rp != new_state.rp { self.changes.push("rp".to_string()); }
+        if self.emulator.sp != new_state.sp { self.changes.push("sp".to_string()); }
+        if self.emulator.ef != new_state.ef { self.changes.push("ef".to_string()); }
+
+        // Swap out state
+        self.emulator = new_state;
+
+        Ok(())
+    }
 }
 
 fn draw(f: &mut Frame, state: &ApplicationState) {
