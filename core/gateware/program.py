55ac37516bc90a14c0c93eb66028dc583d329f01
Aaron Christiansen <aaronc20000@gmail.com>
2023-11-23 23:34:41 +0000

    Add proper TinyFPGA top

--- core/gateware/program.py
+++ core/gateware/program.py
@@ -1,5 +1,4 @@
-from src.modules.top import Top
-from src.platform.tinyfpga_bx import TinyFPGABXSerialProgPlatform
+from src.platform.tinyfpga_bx import TinyFPGABXSerialProgPlatform, TinyFPGABXTop
 import sys
 
 match sys.argv:
@@ -12,4 +11,8 @@ match sys.argv:
         print(f"Usage: {name} program/build")
         sys.exit(1)
 
-TinyFPGABXSerialProgPlatform().build(Top(), do_program=program)
+INSTRUCTIONS = [int(x, 16) for x in """
+4300 1217 1A00 1320 1B00 6212 6212 6212 6213 6213 6213 6212 6212 6212 142D 1C00 6214 6214 6214 6214 6214 6214 60EA 2195 142D 1C00 4000 6214 4000 6214 21D5 6218 2195 142D 1C00 4000 6214 6214 6214 4000 6214 6214 6214 21D5 6218 16FF 1EFF 4826 5016 5000 61FC 6218
+""".split()]
+
+TinyFPGABXSerialProgPlatform().build(TinyFPGABXTop(instructions=INSTRUCTIONS), do_program=program)
--- core/gateware/src/platform/tinyfpga_bx.py
+++ core/gateware/src/platform/tinyfpga_bx.py
@@ -1,4 +1,6 @@
 import os, subprocess
+from amaranth import *
+from ..modules.core import Core
 from amaranth_boards.tinyfpga_bx import TinyFPGABXPlatform
 
 class TinyFPGABXSerialProgPlatform(TinyFPGABXPlatform):
@@ -9,3 +11,43 @@ class TinyFPGABXSerialProgPlatform(TinyFPGABXPlatform):
         tinyprog = os.environ.get("TINYPROG", "tinyprog")
         with products.extract("{}.bin".format(name)) as bitstream_filename:
             subprocess.check_call([tinyprog, "-p", bitstream_filename, "--pyserial"])
+
+
+class TinyFPGABXTop(Elaboratable):
+    # The number of RAM words available.
+    #
+    # Trial-and-errored to be the largest with synthesises successfully.
+    # Seems about right, though - TinyFPGA BX has:
+    #       128k bits of block RAM
+    #    -> 16k bytes
+    #    -> 0x4000 bytes
+    #    -> 0x2000 words
+    MEMORY_DEPTH = 0x1E00
+
+    def __init__(self, instructions):
+        self.mem_init = [0 for _ in range(TinyFPGABXTop.MEMORY_DEPTH)]
+        for i, ins in enumerate(instructions):
+            self.mem_init[i] = ins
+
+    def elaborate(self, platform) -> Module:
+        m = Module()
+
+        mem = Memory(width=Core.DATA_WIDTH, depth=TinyFPGABXTop.MEMORY_DEPTH, init=self.mem_init)
+        mem_read = mem.read_port()
+        mem_write = mem.write_port()
+        m.d.comb += mem_write.addr.eq(mem_read.addr)
+
+        self.core = Core(
+            mem_addr=mem_read.addr,
+            mem_read_data=mem_read.data,
+            mem_read_en=mem_read.en,
+            mem_write_data=mem_write.data,
+            mem_write_en=mem_write.en,
+            debug_led=platform.request("led"),
+        )
+
+        m.submodules.mem_read = mem_read
+        m.submodules.mem_write = mem_write
+        m.submodules.core = self.core
+
+        return m
