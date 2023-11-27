d611595489b07cb091aa8ef5dde93a73ff64a9f1
Aaron Christiansen <aaronc20000@gmail.com>
2023-11-27 20:58:59 +0000

    Remove distinction between statement and  terminator
    As I started writing analysis logic, this only looked like it would
    cause headaches!

--- lang/lib/backend/src/ir/mod.rs
+++ lang/lib/backend/src/ir/mod.rs
@@ -6,9 +6,6 @@ use std::collections::HashMap;
 
 pub use stmt::*;
 
-mod term;
-pub use term::*;
-
 #[derive(Debug, Clone)]
 pub struct Function {
     pub name: String,
@@ -16,39 +13,44 @@ pub struct Function {
     pub variables: HashMap<VariableId, Variable>,
 }
 
+impl Function {
+    pub fn get_statement(&self, id: StatementId) -> &Statement {
+        let StatementId(block_id, index) = id;
+        let block = self.get_basic_block(block_id);
+        &block.statements[index]
+    }
+
+    pub fn get_basic_block(&self, id: BasicBlockId) -> &BasicBlock {
+        self.blocks.get(&id).expect("missing basic block")
+    }
+}
+
 /// Uniquely identifies a [BasicBlock] within a [Function].
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct BasicBlockId(usize);
 
-/// A block of [Statement]s which execute sequentially, ending with a [TerminatorInstruction] which
-/// will branch to another [BasicBlock] or leave the enclosing [Function].
+/// A block of [Statement]s which execute sequentially, where the last is a terminator which will
+/// branch to another [BasicBlock] or leave the enclosing [Function].
 #[derive(Debug, Clone)]
 pub struct BasicBlock {
     pub id: BasicBlockId,
     pub statements: Vec<Statement>,
-    pub terminator: TerminatorInstruction,
 }
 
+/// Uniquely identifies a [Statement] within a [Function].
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct StatementId(BasicBlockId, usize);
+
 /// Uniquely identifies a [Variable] within a [Function].
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct VariableId(usize);
 
-/// Contains the value produced by a [StatementInstruction]. As this IR is in SSA form, any variable
-/// is assigned to exactly once.
+/// Contains the value produced by an [Instruction]. As this IR is in SSA form, any variable is
+/// assigned to exactly once.
 #[derive(Debug, Clone)]
 pub struct Variable {
     pub id: VariableId,
     pub ty: Type,
-}
-
-/// Breaks out shared functionality between [StatementInstruction] and [TerminatorInstruction].
-pub trait Instruction {
-    /// Returns the IDs of any variables which are read by this instruction. Used to implement
-    /// liveness analysis.
-    /// 
-    /// Should not include the result, if the instruction is a statement - only variables which are
-    /// read as part of the instruction itself.
-    fn referenced_variables(&self) -> Vec<VariableId>;
 }
 
 /// Describes the type of an IR [Variable].
--- lang/lib/backend/src/ir/stmt.rs
+++ lang/lib/backend/src/ir/stmt.rs
@@ -1,34 +1,55 @@
-use super::{Variable, Instruction, VariableId, Type, IntegerSize};
+use super::{Variable, VariableId, Type, IntegerSize};
 
-/// Wraps a [StatementInstruction] and the [Variable] which it assigns to.
+/// Wraps an [Instruction] and the [Variable] which it assigns to.
 #[derive(Debug, Clone)]
 pub struct Statement {
     pub result: Variable,
-    pub instruction: StatementInstruction,
+    pub instruction: Instruction,
 }
 
 /// An instruction which performs some computation and produces a result, which the enclosing
 /// [Statement] will assign to a [Variable].
 #[derive(Debug, Clone)]
-pub struct StatementInstruction {
-    pub kind: StatementInstructionKind,
+pub struct Instruction {
+    pub kind: InstructionKind,
 }
 
-/// All possible [StatementInstruction] operations.
+/// All possible [Instruction] operations.
 #[derive(Debug, Clone)]
-pub enum StatementInstructionKind {
+pub enum InstructionKind {
     /// Evaluates to a constant value.
     Constant(ConstantValue),
 
     /// Adds together two integer values, of the same type.
     Add(VariableId, VariableId),
+
+    /// Returns from the enclosing function, with a value if it is non-void.
+    Return(Option<VariableId>),
 }
 
-impl Instruction for StatementInstruction {
-    fn referenced_variables(&self) -> Vec<VariableId> {
+impl Instruction {
+    /// Whether this instruction is a terminator.
+    /// 
+    /// Every basic block must end with a terminator, and conversely terminators can only appear at
+    /// the end of basic blocks.
+    pub fn is_terminator(&self) -> bool {
         match self.kind {
-            StatementInstructionKind::Constant(_) => vec![],
-            StatementInstructionKind::Add(l, r) => vec![l, r],
+            InstructionKind::Return(_) => true,
+
+            _ => false,
+        }
+    }
+
+    /// Returns the IDs of any variables which are read by this instruction. Used to implement
+    /// liveness analysis.
+    /// 
+    /// Should not include the result, if the instruction is a statement - only variables which are
+    /// read as part of the instruction itself.
+    pub fn referenced_variables(&self) -> Vec<VariableId> {
+        match self.kind {
+            InstructionKind::Constant(_) => vec![],
+            InstructionKind::Add(l, r) => vec![l, r],
+            InstructionKind::Return(r) => r.into_iter().collect(),
         }
     }
 }
