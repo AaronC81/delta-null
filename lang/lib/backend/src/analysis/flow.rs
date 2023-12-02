83c30e2bde3faab3ae429c08a614dd9a51530d47
Aaron Christiansen <aaronc20000@gmail.com>
2023-12-02 17:33:53 +0000

    Add some new CFG analysis tools

--- lang/lib/backend/src/analysis/flow.rs
+++ lang/lib/backend/src/analysis/flow.rs
@@ -1,9 +1,14 @@
 use std::collections::{HashMap, HashSet};
 
+use maplit::hashset;
+
 use crate::ir::{Function, BasicBlockId};
 
 #[derive(Debug, Clone)]
 pub struct ControlFlowGraph {
+    /// The entry point of the function represented by this CFG.
+    entry_point: BasicBlockId,
+
     /// Maps a block to the other blocks that it could branch to.
     outgoing: HashMap<BasicBlockId, HashSet<BasicBlockId>>,
 
@@ -20,6 +25,7 @@ impl ControlFlowGraph {
             .map(|id| (*id, HashSet::new()))
             .collect::<HashMap<_, _>>();
         let mut cfg = ControlFlowGraph {
+            entry_point: *func.blocks.keys().next().unwrap(),
             incoming: initial_map.clone(),
             outgoing: initial_map,
         };
@@ -46,6 +52,75 @@ impl ControlFlowGraph {
     /// Given a block, returns the set of blocks which could branch from themselves to it.
     pub fn incoming_branches(&self, block: BasicBlockId) -> &HashSet<BasicBlockId> {
         self.incoming.get(&block).unwrap()
+    }
+
+    /// Performs a breadth-first traversal of the basic blocks in the graph, and returns the blocks
+    /// encountered for the first time at each depth.
+    /// 
+    /// No block ID is returned twice, and some blocks may not appear at all if there is no way to
+    /// reach them. The first "layer" always contains just one block, the entry point.
+    pub fn breadth_first(&self) -> Vec<HashSet<BasicBlockId>> {
+        let mut result = vec![hashset! { self.entry_point }];
+        let mut all_encountered = hashset! { self.entry_point };
+
+        loop {
+            let sources = result.last().unwrap();
+            let mut next_layer = HashSet::new();
+
+            for source in sources {
+                next_layer.extend(
+                    self.outgoing_branches(*source)
+                        .difference(&all_encountered)
+                )
+            }
+
+            if next_layer.is_empty() {
+                // We reached the end!
+                return result;
+            }
+
+            all_encountered.extend(next_layer.iter());
+            result.push(next_layer);
+        }
+    }
+
+    /// Performs a depth-first traversal of the basic blocks in the graph, and returns the blocks
+    /// encountered in order.
+    /// 
+    /// There are multiple orders in which a graph can be depth-first traversed, and the order which
+    /// this returns is not guaranteed.
+    /// 
+    /// No block ID is returned twice, and some blocks may not appear at all if there is no way to
+    /// reach them.
+    pub fn depth_first(&self) -> Vec<BasicBlockId> {
+        let mut result = vec![];
+        let mut pending = vec![self.entry_point];
+
+        loop {
+            // Take a block from the top of the pending stack
+            let Some(next_to_traverse) = pending.pop() else {
+                return result;
+            };
+            result.push(next_to_traverse);
+
+            // Push pending searches for descendant blocks we haven't searched yet
+            let mut children = self.outgoing_branches(next_to_traverse)
+                .iter()
+                .filter(|b| !result.contains(&b))
+                .collect::<Vec<_>>();
+            children.sort(); // Give deterministic ordering for tests
+            pending.extend(children);
+        }
+    }
+
+    /// Calculates a topological-style ordering for the basic blocks, using the information in this
+    /// graph.
+    /// 
+    /// When a block appears in this ordering, all blocks which *forward*-branch to it have appeared
+    /// before. The ordering in which "equivalent" branches appear is not defined.
+    pub fn ordering(&self) -> Vec<BasicBlockId> {
+        // I think a depth-first search naturally has the required properties?
+        self.depth_first()
     }
 
     /// Returns the set of the blocks which don't branch to another block, instead terminating in
@@ -135,5 +210,55 @@ mod test {
         assert_eq!(cfg.incoming_branches(ids[6]), &hashset!{ ids[4], ids[5] });
 
         assert_eq!(cfg.leaf_blocks(), hashset!{ ids[6] });
+    }
+
+    #[test]
+    fn test_block_traversal_ordering() {
+        let mut func = FunctionBuilder::new("foo");
+
+        // The graph will be         But the expected 
+        // constructed like          ordering is...
+        // this:
+        //
+        //        3                  = 0
+        //        v
+        //    .-- 2 <-.              = 1
+        //    |   v   0              = 3
+        //    |   4 --^              = 2
+        //    |
+        //    '-> 1                  = 4
+
+        let (ids, mut blocks) = func.new_basic_blocks(5);
+        blocks[3].add_terminator(Instruction::new(InstructionKind::Branch(ids[2])));
+        let condition = blocks[2].add_instruction(Instruction::new(InstructionKind::Constant(ConstantValue::Boolean(true))));
+        blocks[2].add_terminator(Instruction::new(InstructionKind::ConditionalBranch { condition, true_block: ids[1], false_block: ids[4] }));
+        blocks[4].add_terminator(Instruction::new(InstructionKind::Branch(ids[0])));
+        blocks[0].add_terminator(Instruction::new(InstructionKind::Branch(ids[2])));
+        blocks[1].add_terminator(Instruction::new(InstructionKind::Return(None)));
+        func.finalize_blocks(blocks);
+        let func = func.finalize();
+
+        let mut cfg = ControlFlowGraph::generate(&func);
+        cfg.entry_point = ids[3];
+    
+        // Test traversal orders
+        assert_eq!(
+            vec![
+                hashset! { ids[3] },
+                hashset! { ids[2] },
+                hashset! { ids[1], ids[4] },
+                hashset! { ids[0] },
+            ],
+            cfg.breadth_first(),
+        );
+        assert_eq!(
+            vec![ids[3], ids[2], ids[4], ids[0], ids[1]],
+            cfg.depth_first(),
+        );
+
+        assert_eq!(
+            vec![ids[3], ids[2], ids[4], ids[0], ids[1]],
+            cfg.ordering(),
+        )
     }
 }
