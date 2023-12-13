fa491cfbc59c0376e167d939436e40e473a16f23
Aaron Christiansen <aaronc20000@gmail.com>
2023-12-13 21:43:20 +0000

    Add compiler `--to-ir` option

--- lang/bin/compiler/src/main.rs
+++ lang/bin/compiler/src/main.rs
@@ -1,8 +1,9 @@
-use std::{fs::OpenOptions, io::{Write, stdout}};
+use std::{fs::OpenOptions, io::{Write, stdout}, process::exit};
 
 use clap::Parser as ClapParser;
 use clap_stdin::FileOrStdin;
 use delta_null_core_instructions::ToAssembly;
+use delta_null_lang_backend::ir::{PrintIR, PrintOptions};
 use delta_null_lang_backend_core::compile_module;
 use delta_null_lang_frontend::code_to_module;
 
@@ -14,6 +15,10 @@ struct Args {
     /// Output file, or stdout if not given
     #[arg(short = 'o', long)]
     output_file: Option<String>,
+
+    /// Print IR and stop, without executing the compiler backend
+    #[arg(long)]
+    to_ir: bool,
 }
 
 fn main() {
@@ -22,6 +27,14 @@ fn main() {
     // Read and compile
     let input = args.input.to_string();
     let module = code_to_module(&input).unwrap();
+    if args.to_ir {
+        println!("{}", module.functions.into_iter()
+            .map(|f| f.print_ir(&PrintOptions::new()))
+            .collect::<Vec<_>>()
+            .join("\n"));
+        exit(0)
+    }
+
     let asm = compile_module(&module).unwrap();
 
     // Open output
