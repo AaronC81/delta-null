a7f3941a2d0120752a894585effa2af1c6c32169
Aaron Christiansen <aaronc20000@gmail.com>
2024-04-20 18:24:33 +0100

    Update program.py to use hardcoded Colorlight stuff - will make this controllable later!

--- core/gateware/program.py
+++ core/gateware/program.py
@@ -1,4 +1,6 @@
-from src.platform.tinyfpga_bx import TinyFPGABXSerialProgPlatform, TinyFPGABXTop, TinyFPGABXMemoryMap
+#from src.platform.tinyfpga_bx import TinyFPGABXSerialProgPlatform, TinyFPGABXTop, TinyFPGABXMemoryMap
+from src.platform.colorlight_i5 import ColorlightI5Top, ColorlightI5MemoryMap
+from src.boards.colorlight_i5_r7_0_ext_board import Colorlighti5R70ExtensionBoardPlatform
 import sys, os
 from tests.helpers import assemble
 
@@ -12,12 +14,12 @@ match sys.argv:
         print(f"Usage: {name} program/build")
         sys.exit(1)
 
-ASM_FILE = os.path.join(os.path.dirname(__file__), "..", "examples", "sos_blink.dna")
+ASM_FILE = "/Users/aaron/Source/delta-null/modern_blink.dna" #os.path.join(os.path.dirname(__file__), "..", "examples", "sos_blink.dna")
 
 # Assemble code from file
 with open(ASM_FILE) as f:
     code = f.read()
-instructions = assemble(code, start_address=TinyFPGABXMemoryMap.RAM_START)
+instructions = assemble(code, start_address=ColorlightI5MemoryMap.RAM_START)
 
 # Build/program
-TinyFPGABXSerialProgPlatform().build(TinyFPGABXTop(instructions=instructions), do_program=program)
+Colorlighti5R70ExtensionBoardPlatform().build(ColorlightI5Top(instructions=instructions))
