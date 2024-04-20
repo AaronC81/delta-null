dd0d3c69f6243a8b57118555842b45b520f89bd9
Aaron Christiansen <aaronc20000@gmail.com>
2024-04-20 19:05:56 +0100

    Refactor out common parts of platforms

---
+++ core/gateware/src/platform/base.py
@@ -0,0 +1,140 @@
+from amaranth import *
+from amaranth.build import *
+from ..modules.core import Core
+from typing import Optional
+
+class BaseMemoryMap(Elaboratable):
+    # The start address of RAM.
+    RAM_START = None
+    
+    # The number of RAM words available.
+    RAM_DEPTH = None
+
+    # The start address of the HCR.
+    HCR_START = None
+
+    # The total number of GPIO pins available through the HCR.
+    HCR_GPIO_PIN_COUNT = None
+
+    def __init__(self, init_ram, depth):
+        self.init_ram = init_ram
+        self.depth = depth
+
+        self.addr = Signal(Core.DATA_WIDTH)
+        self.read_data = Signal(Core.DATA_WIDTH)
+        self.write_data = Signal(Core.DATA_WIDTH)
+        self.read_en = Signal()
+        self.write_en = Signal()
+
+        self.hcr_gpio_o =  Signal(type(self).HCR_GPIO_PIN_COUNT)
+        self.hcr_gpio_i =  Signal(type(self).HCR_GPIO_PIN_COUNT)
+        self.hcr_gpio_oe = Signal(type(self).HCR_GPIO_PIN_COUNT)
+
+    def bind_hcr_peripherals(self, platform: Platform, m: Module):
+        raise NotImplementedError()
+
+    def elaborate(self, platform: Platform) -> Module:
+        m = Module()
+
+        # Instantiate RAM
+        self.ram = Memory(width=Core.DATA_WIDTH, depth=self.depth, init=self.init_ram)
+        mem_read = self.ram.read_port()
+        mem_write = self.ram.write_port()
+        m.d.comb += mem_write.addr.eq(mem_read.addr)
+
+        m.submodules.mem_read = mem_read
+        m.submodules.mem_write = mem_write
+
+        if platform is not None:
+            self.bind_hcr_peripherals(platform, m)
+
+        with m.If(self.addr.matches("1111 ---- ---- ----")):
+            # Handled by HCR (0xF---)
+            hcr_rel_addr = self.addr - type(self).HCR_START
+
+            # Check write first, that takes "priority"
+            # (Also, read_en is fixed high on some platforms)
+            with m.If(self.write_en):
+                with m.Switch(hcr_rel_addr):
+                    # === GPIO ===
+                    with m.Case(0x10): # Mode configuration (low)
+                        m.d.sync += self.hcr_gpio_oe[0:16].eq(self.write_data)
+                    with m.Case(0x11): # Mode configuration (high)
+                        m.d.sync += self.hcr_gpio_oe[16:32].eq(self.write_data)
+                    with m.Case(0x12): # Output (low)
+                        m.d.sync += self.hcr_gpio_o[0:16].eq(self.write_data)
+                    with m.Case(0x13): # Output (high)
+                        m.d.sync += self.hcr_gpio_o[16:32].eq(self.write_data)
+                    # TODO: input
+
+            with m.Elif(self.read_en):
+                with m.Switch(hcr_rel_addr):
+                    # === Metadata ===
+                    with m.Case(0x0): # Magic number
+                        m.d.comb += self.read_data.eq(0xF90A)
+                    with m.Case(0x1): # Harness indicator
+                        m.d.comb += self.read_data.eq(0)
+
+                    # === GPIO ===
+                    with m.Case(0x10): # Mode configuration (low)
+                        m.d.comb += self.read_data.eq(self.hcr_gpio_oe[0:16])
+                    with m.Case(0x11): # Mode configuration (high)
+                        m.d.comb += self.read_data.eq(self.hcr_gpio_oe[16:32])
+                    with m.Case(0x12): # Output (low)
+                        m.d.comb += self.read_data.eq(self.hcr_gpio_o[0:16])
+                    with m.Case(0x13): # Output (high)
+                        m.d.comb += self.read_data.eq(self.hcr_gpio_o[16:32])
+                    # TODO: input
+
+                    # Something we don't know!
+                    with m.Default():
+                        m.d.comb += self.read_data.eq(0x0BAD)
+        
+        with m.Else():
+            # Forward to RAM
+            m.d.comb += [
+                mem_read.addr.eq(self.addr - type(self).RAM_START),
+                self.read_data.eq(mem_read.data),
+                mem_write.data.eq(self.write_data),
+                mem_write.en.eq(self.write_en),
+            ]
+            if isinstance(mem_read.en, Signal):
+                m.d.comb += mem_read.en.eq(self.read_en)
+
+        return m
+
+
+class BaseTop(Elaboratable):
+    MEMORY_MAP = None
+    INITIAL_SP = None
+    INITIAL_IP = None
+
+    def __init__(self, instructions, depth=None):
+        if depth is None:
+            depth = type(self).MEMORY_MAP.RAM_DEPTH
+        self.depth = depth
+
+        self.mem_init = [0 for _ in range(depth)]
+        for i, ins in enumerate(instructions):
+            self.mem_init[i] = ins
+
+    def elaborate(self, platform) -> Module:
+        m = Module()
+
+        self.mem = mem = type(self).MEMORY_MAP(self.mem_init, self.depth)
+
+        self.core = Core(
+            mem_addr=mem.addr,
+            mem_read_data=mem.read_data,
+            mem_read_en=mem.read_en,
+            mem_write_data=mem.write_data,
+            mem_write_en=mem.write_en,
+
+            initial_sp=type(self).INITIAL_SP,
+            initial_ip=type(self).INITIAL_IP,
+        )
+
+        m.submodules.mem = mem
+        m.submodules.core = self.core
+
+        return m
--- core/gateware/src/platform/tinyfpga_bx.py
+++ core/gateware/src/platform/tinyfpga_bx.py
@@ -2,6 +2,7 @@ import os, subprocess
 from amaranth import *
 from amaranth.build import *
 from ..modules.core import Core
+from .base import BaseMemoryMap, BaseTop
 from amaranth_boards.tinyfpga_bx import TinyFPGABXPlatform
 
 class TinyFPGABXSerialProgPlatform(TinyFPGABXPlatform):
@@ -13,8 +14,7 @@ class TinyFPGABXSerialProgPlatform(TinyFPGABXPlatform):
         with products.extract("{}.bin".format(name)) as bitstream_filename:
             subprocess.check_call([tinyprog, "-p", bitstream_filename, "--pyserial"])
 
-
-class TinyFPGABXMemoryMap(Elaboratable):
+class TinyFPGABXMemoryMap(BaseMemoryMap):
     # The start address of RAM.
     RAM_START = 0x1000
     
@@ -34,139 +34,31 @@ class TinyFPGABXMemoryMap(Elaboratable):
     # The total number of GPIO pins available through the HCR.
     HCR_GPIO_PIN_COUNT = 32
 
-    def __init__(self, init_ram, depth):
-        self.init_ram = init_ram
-        self.depth = depth
+    def bind_hcr_peripherals(self, platform: Platform, m: Module):
+        # Create a new resource for every mapped hardware GPIO pin
+        # (There is a "connector" defined already, but I don't think we have direct access to
+        #  those...)
+        # It's possible to specify multiple pins for one resource, but that only gives us a single
+        # output-enable for every pin. We need per-pin control.
+        resources = []
+        for i in range(1, TinyFPGABXMemoryMap.HCR_GPIO_PIN_COUNT):
+            resources.append(Resource(f"gpio{i}", 0,
+                Pins(f"{i}", dir="io", conn=("gpio", 0))                          
+            ))
+        platform.add_resources(resources)
 
-        self.addr = Signal(Core.DATA_WIDTH)
-        self.read_data = Signal(Core.DATA_WIDTH)
-        self.write_data = Signal(Core.DATA_WIDTH)
-        self.read_en = Signal()
-        self.write_en = Signal()
-
-        self.hcr_gpio_o = Signal(TinyFPGABXMemoryMap.HCR_GPIO_PIN_COUNT)
-        self.hcr_gpio_i = Signal(TinyFPGABXMemoryMap.HCR_GPIO_PIN_COUNT)
-        self.hcr_gpio_oe = Signal(TinyFPGABXMemoryMap.HCR_GPIO_PIN_COUNT)
-
-    def elaborate(self, platform: Platform) -> Module:
-        m = Module()
-
-        # Instantiate RAM
-        self.ram = Memory(width=Core.DATA_WIDTH, depth=self.depth, init=self.init_ram)
-        mem_read = self.ram.read_port()
-        mem_write = self.ram.write_port()
-        m.d.comb += mem_write.addr.eq(mem_read.addr)
-
-        m.submodules.mem_read = mem_read
-        m.submodules.mem_write = mem_write
-
-        if platform is not None:
-            # Create a new resource for every mapped hardware GPIO pin
-            # (There is a "connector" defined already, but I don't think we have direct access to
-            #  those...)
-            # It's possible to specify multiple pins for one resource, but that only gives us a single
-            # output-enable for every pin. We need per-pin control.
-            resources = []
-            for i in range(1, TinyFPGABXMemoryMap.HCR_GPIO_PIN_COUNT):
-                resources.append(Resource(f"gpio{i}", 0,
-                    Pins(f"{i}", dir="io", conn=("gpio", 0))                          
-                ))
-            platform.add_resources(resources)
-
-            # Bind each IO pin
-            # Pin 0 is LED (only output), the rest are GPIO (input/output)
-            m.d.comb += platform.request("led").eq(self.hcr_gpio_o[0])
-            for i in range(1, TinyFPGABXMemoryMap.HCR_GPIO_PIN_COUNT):
-                pin = platform.request(f"gpio{i}")
-                m.d.comb += [
-                    self.hcr_gpio_i[i].eq(pin.i),
-                    pin.oe.eq(self.hcr_gpio_oe[i]),
-                    pin.o.eq(self.hcr_gpio_o[i]),
-                ]
-
-        with m.If(self.addr.matches("1111 ---- ---- ----")):
-            # Handled by HCR (0xF---)
-            hcr_rel_addr = self.addr - TinyFPGABXMemoryMap.HCR_START
-
-            # Check write first, that takes "priority"
-            # (Also, read_en is fixed high on some platforms)
-            with m.If(self.write_en):
-                with m.Switch(hcr_rel_addr):
-                    # === GPIO ===
-                    with m.Case(0x10): # Mode configuration (low)
-                        m.d.sync += self.hcr_gpio_oe[0:16].eq(self.write_data)
-                    with m.Case(0x11): # Mode configuration (high)
-                        m.d.sync += self.hcr_gpio_oe[16:32].eq(self.write_data)
-                    with m.Case(0x12): # Output (low)
-                        m.d.sync += self.hcr_gpio_o[0:16].eq(self.write_data)
-                    with m.Case(0x13): # Output (high)
-                        m.d.sync += self.hcr_gpio_o[16:32].eq(self.write_data)
-                    # TODO: input
-
-            with m.Elif(self.read_en):
-                with m.Switch(hcr_rel_addr):
-                    # === Metadata ===
-                    with m.Case(0x0): # Magic number
-                        m.d.comb += self.read_data.eq(0xF90A)
-                    with m.Case(0x1): # Harness indicator
-                        m.d.comb += self.read_data.eq(0)
-
-                    # === GPIO ===
-                    with m.Case(0x10): # Mode configuration (low)
-                        m.d.comb += self.read_data.eq(self.hcr_gpio_oe[0:16])
-                    with m.Case(0x11): # Mode configuration (high)
-                        m.d.comb += self.read_data.eq(self.hcr_gpio_oe[16:32])
-                    with m.Case(0x12): # Output (low)
-                        m.d.comb += self.read_data.eq(self.hcr_gpio_o[0:16])
-                    with m.Case(0x13): # Output (high)
-                        m.d.comb += self.read_data.eq(self.hcr_gpio_o[16:32])
-                    # TODO: input
-
-                    # Something we don't know!
-                    with m.Default():
-                        m.d.comb += self.read_data.eq(0x0BAD)
-        
-        with m.Else():
-            # Forward to RAM
+        # Bind each IO pin
+        # Pin 0 is LED (only output), the rest are GPIO (input/output)
+        m.d.comb += platform.request("led").eq(self.hcr_gpio_o[0])
+        for i in range(1, TinyFPGABXMemoryMap.HCR_GPIO_PIN_COUNT):
+            pin = platform.request(f"gpio{i}")
             m.d.comb += [
-                mem_read.addr.eq(self.addr - TinyFPGABXMemoryMap.RAM_START),
-                self.read_data.eq(mem_read.data),
-                mem_write.data.eq(self.write_data),
-                mem_write.en.eq(self.write_en),
+                self.hcr_gpio_i[i].eq(pin.i),
+                pin.oe.eq(self.hcr_gpio_oe[i]),
+                pin.o.eq(self.hcr_gpio_o[i]),
             ]
-            if isinstance(mem_read.en, Signal):
-                m.d.comb += mem_read.en.eq(self.read_en)
 
-        return m
-
-
-class TinyFPGABXTop(Elaboratable):
-    def __init__(self, instructions, depth=None):
-        if depth is None:
-            depth = TinyFPGABXMemoryMap.RAM_DEPTH
-        self.depth = depth
-
-        self.mem_init = [0 for _ in range(depth)]
-        for i, ins in enumerate(instructions):
-            self.mem_init[i] = ins
-
-    def elaborate(self, platform) -> Module:
-        m = Module()
-
-        self.mem = mem = TinyFPGABXMemoryMap(self.mem_init, self.depth)
-
-        self.core = Core(
-            mem_addr=mem.addr,
-            mem_read_data=mem.read_data,
-            mem_read_en=mem.read_en,
-            mem_write_data=mem.write_data,
-            mem_write_en=mem.write_en,
-
-            initial_sp=0x2E00,
-            initial_ip=0x1000,
-        )
-
-        m.submodules.mem = mem
-        m.submodules.core = self.core
-
-        return m
+class TinyFPGABXTop(BaseTop):
+    MEMORY_MAP = TinyFPGABXMemoryMap
+    INITIAL_SP = 0x2E00
+    INITIAL_IP = 0x1000
