f587d18d2af4650ae3fd846c502b284c36cc593b
Aaron Christiansen <aaronc20000@gmail.com>
2024-06-12 23:59:57 +0100

    Compile items in order (except for the entry point)
    This will make more sense when we have an assembly item - we want that to appear roughly at the point of declaration

--- lang/lib/backend-core/src/lib.rs
+++ lang/lib/backend-core/src/lib.rs
@@ -6,7 +6,7 @@
 use codegen::FunctionGenerator;
 use delta_null_core_assembler::{AssemblyItem, AssemblyItemKind, BuildError};
 use delta_null_core_instructions::InstructionOpcode;
-use delta_null_lang_backend::{analysis::{flow::ControlFlowGraph, liveness::liveness_analysis, misc::is_leaf_function}, ir::{Function, Module}};
+use delta_null_lang_backend::{analysis::{flow::ControlFlowGraph, liveness::liveness_analysis, misc::is_leaf_function}, ir::{Function, Module, ModuleItem}};
 use peephole::peephole_optimise;
 use reg_alloc::allocate;
 
@@ -25,27 +25,30 @@ pub fn compile_module(module: &Module) -> Result<Vec<AssemblyItem>, Vec<BuildErr
 
     // Find entry function and compile it first, as that's how our program's entry point currently
     // works
-    let (entry_funcs, other_funcs): (Vec<_>, Vec<_>) = module.functions().partition(|f| &f.name == entry);
+    let (entry_funcs, other_items): (Vec<_>, Vec<_>) = module.items.iter()
+        .partition(|i|
+            if let ModuleItem::Function(f) = i && &f.name == entry { true } else { false }
+        );
     if entry_funcs.len() != 1 {
         panic!("expected 1 entry point function named {entry}, but found {}", entry.len())
     }
 
     // Compile and concatenate all
     let mut items = vec![];
-    for func in entry_funcs.iter().chain(other_funcs.iter()) {
-        items.extend(compile_function(func)?);
-    }
+    for item in entry_funcs.iter().chain(other_items.iter()) {
+        match item {
+            ModuleItem::Function(func) => items.extend(compile_function(func)?),
+            ModuleItem::Data(datum) => {
+                let size_after_first_word = datum.ty.word_size() - 1;
 
-    // Create data slots
-    for datum in module.data() {
-        let size_after_first_word = datum.ty.word_size() - 1;
-
-        items.push(AssemblyItem {
-            labels: vec![datum.name.clone()],
-            kind: AssemblyItemKind::Instruction(InstructionOpcode::Nop, vec![]),
-        });
-        for _ in 0..size_after_first_word {
-            items.push(AssemblyItem::new_instruction(InstructionOpcode::Nop, &[]));
+                items.push(AssemblyItem {
+                    labels: vec![datum.name.clone()],
+                    kind: AssemblyItemKind::Instruction(InstructionOpcode::Nop, vec![]),
+                });
+                for _ in 0..size_after_first_word {
+                    items.push(AssemblyItem::new_instruction(InstructionOpcode::Nop, &[]));
+                }        
+            }
         }
     }
 
